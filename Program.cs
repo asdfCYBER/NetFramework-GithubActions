0e0735a422ef47b9d417b7c779b02447805bc893
asdfCYBER <asdfCYBER@outlook.com>
2020-12-19 16:08:01 +0100

    Add build action

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Presentation;
 
 namespace NetFramework_GithubActions
 {
@@ -17,6 +20,8 @@ namespace NetFramework_GithubActions
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            PresentationDocument document = PresentationDocument.Create("test.pptx", PresentationDocumentType.Presentation);
         }
     }
 }
